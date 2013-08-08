39311f077f997ee364abe989606347c4de055f37
KEvin Wang <kvn.a.wng@gmail.com>
2013-08-08 02:13:48 -0700

    Signing in works.  HTML prettified before parsed by mechanize
    Got mechanize to recognize the form by prettifying the html with
    beautifulsoup before parsing with mechanize.  Signing in works now.

--- salty.py
+++ salty.py
@@ -1,8 +1,11 @@
+import config as cf
 import requests
 import mechanize
 from BeautifulSoup import *
 br = mechanize.Browser()
 import cookielib
+
+"""technical stuff"""
 cj = cookielib.LWPCookieJar()
 br.set_cookiejar(cj)
 br.set_handle_equiv(True)
@@ -10,10 +13,10 @@ br.set_handle_gzip(True)
 br.set_handle_redirect(True)
 br.set_handle_referer(True)
 br.set_handle_robots(False)
- 
+
 # Follows refresh 0 but not hangs on refresh > 0
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
- 
+
 # Want debugging messages?
 #br.set_debug_http(True)
 #br.set_debug_redirects(True)
@@ -21,20 +24,26 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
  
 # User-Agent (this is cheating, ok?)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+"""end technical stuff"""
+
 url = "http://saltybet.com/authenticate?signin=1"
-
-br.open(url)
-
-"""payload = {'AnSwEr0001':'0.1'}
-r = requests.post(url, payload)
-with open("requests_results.html", "w") as f:
-    f.write(r.content)
-"""
+resp = br.open(url)
+#use BeautifulSoup to prettify the html before running it through mechanize
+soup = BeautifulSoup(resp.get_data())
+resp.set_data(soup.prettify())
+br.set_response(resp)
 br.select_form(nr=0)
-br.form['email'] = ''
-br.form['pword'] = ''
+br['email'] = cf.email
+br['pword'] = cf.pword
 returns = br.submit()
-returns.read()
+url = "http://saltybet.com/stats"
+resp = br.open(url)
+#use BeautifulSoup to prettify the html before running it through mechanize
+soup = BeautifulSoup(resp.get_data())
+resp.set_data(soup.prettify())
+br.set_response(resp)
+print soup
+
 if False:
     i = .01
     while i < 1:
@@ -53,6 +62,8 @@ if False:
         i=i*1.009
         print i
 else:
+    print returns
+    """return
     for ans in SF.findAns([1,2,4,5,11]):
         br.select_form(nr=0)
         br.form['AnSwEr0001']=str(ans)
@@ -67,3 +78,4 @@ else:
         except:
             print "ASDOFIJASDKJKJLOUDNOISES ASD;FOAJS;DLFKJAS;LDKFJ"
         print ans
+"""
