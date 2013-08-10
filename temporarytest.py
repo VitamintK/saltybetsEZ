c9958e858f6b739e6a18b95af662950fa1fd3790
KEvin Wang <kvn.a.wng@gmail.com>
2013-08-10 09:52:33 -0700

    deleted test file

--- temporarytest.py
+++
@@ -1,8 +0,0 @@
-from BeautifulSoup import *
-with open('statspage','r') as p:
-        soup = BeautifulSoup(p.read())
-#find all tournaments with 'dream' in their name
-tournaments = soup.findAll('a')
-for hlink in tournaments:
-    for d in hlink:
-        print '{0}'.format(d)
