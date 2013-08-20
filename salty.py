05a9e1e5a5d74828894f1e51210c0ce9d3e36ddd
KEvin Wang <kvn.a.wng@gmail.com>
2013-08-20 03:28:22 -0700

    Added a plethora of methods - elo rankings, win %
    added
    -retrieving stats page of all dream casts
    -saving stats to file
    -find_matches(): searching for matches fought between 2 contestants
    given by user input from stats file
    -printing all matches played by any given contestants and their
    calculated winrates, and respective Elo ratings
    -calculation of Elo ratings for each contestant, and saving the ratings
    to a file.
    -a function that prints out all Elo ratings from the file
    -a function that returns a given contestant's Elo rating

--- salty.py
+++ salty.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 import config as cf
 import requests
 import mechanize
@@ -5,32 +6,33 @@ from BeautifulSoup import *
 br = mechanize.Browser()
 import cookielib
 import pickle
+from collections import OrderedDict
 #tool to find matchup odds
 
 online = True
 
-"""technical stuff"""
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
-br.set_handle_equiv(True)
-br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+def save_stats():
+    """technical stuff"""
+    cj = cookielib.LWPCookieJar()
+    br.set_cookiejar(cj)
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
 
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    # Follows refresh 0 but not hangs on refresh > 0
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-# Want debugging messages?
-#br.set_debug_http(True)
-#br.set_debug_redirects(True)
-#br.set_debug_responses(True)
- 
-# User-Agent (this is cheating, ok?)
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-"""end technical stuff"""
-
-if online:
+    # Want debugging messages?
+    #br.set_debug_http(True)
+    #br.set_debug_redirects(True)
+    #br.set_debug_responses(True)
+     
+    # User-Agent (this is cheating, ok?)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    """end technical stuff"""
+    
     url = "http://saltybet.com/authenticate?signin=1"
     resp = br.open(url)
     #use BeautifulSoup to prettify the html before running it through mechanize
@@ -47,65 +49,168 @@ if online:
     soup = BeautifulSoup(resp.get_data())
     resp.set_data(soup.prettify())
     br.set_response(resp)
-#assuming all tournaments with 'dream' in their name are mugen ai
-else:
-    with open('statspage','r') as p:
-        soup = BeautifulSoup(p.read())
-#find all tournaments with 'dream' in their name
-tournaments = soup.findAll('a')
-dreams = []
-for hlink in tournaments:
-    #print list(hlink.contents)[0] <-- for some reason this takes forever
-    #print '{0}'.format(hlink.contents[0]) <-- for some reason this works!?!?!
-    tourneyname = '{0}'.format(hlink.contents[0])
-    for word in tourneyname.split():
-        if word == 'Dream':
-            print hlink
-            print tourneyname
-            dreams.append(hlink)
-            break
-#for dream in dreams:
-for link in br.links():
-    print link.text
-    
-#print soup
-if online:
-    #saves soup to file.  unneeded.
+    #assuming all tournaments with 'dream' in their name are mugen ai
+
+    #find all tournaments with 'dream' in their name
+
+    dreams = []
+    for link in br.links():
+        tourneyname = link.text
+        for word in tourneyname.split():
+            if word == 'Dream':
+                print tourneyname
+                dreams.append(link)
+                break
+
+    #iterate through each dream casino to create data
+    matchlist = []
+    for dream in dreams:
+        responsetest = br.follow_link(dream)
+        responsoup = BeautifulSoup(responsetest.get_data())
+        matches = responsoup.findAll('tr')
+        for match in matches:
+            span = match.findAll('span')
+            try:
+                matchlist.append((span[0].text, span[1].text, span[2].text))
+            except:
+                try:
+                    matchlist.append((span[0].text, span[1].text))
+                except:
+                    print span
+
+    with open("matches",'w') as p:
+        pickle.dump(matchlist,p)
+
     with open('statspage','w') as p:
         p.write(str(soup))
-if False:
-    i = .01
-    while i < 1:
-        br.select_form(nr=0)
-        br.form['AnSwEr0001']=str(i)
-        rq = br.form.click("submitAnswers")
-        asdf = br.open(rq)
-        #print asdf.read()
-        soup1 = BeautifulSoup(asdf.read())
-        try:
-            #print soup1.prettify()
-            resul = soup1.find(None,"ResultsWithError")
-            print resul
-        except:
-            print "ASDOFIJASDKJKJLOUDNOISES ASD;FOAJS;DLFKJAS;LDKFJ"
-        i=i*1.009
-        print i
-else:
-    pass
-    #print returns
-    """return
-    for ans in SF.findAns([1,2,4,5,11]):
-        br.select_form(nr=0)
-        br.form['AnSwEr0001']=str(ans)
-        rq = br.form.click("submitAnswers")
-        asdf = br.open(rq)
-        #print asdf.read()
-        soup1 = BeautifulSoup(asdf.read())
-        try:
-            #print soup1.prettify()
-            resul = soup1.find(None,"ResultsWithError")
-            print resul
-        except:
-            print "ASDOFIJASDKJKJLOUDNOISES ASD;FOAJS;DLFKJAS;LDKFJ"
-        print ans
-"""
+
+    print "stats saved!"
+
+def open_matches():
+    with open('matches','r') as p:
+        matches = pickle.load(p)
+    return matches
+
+def find_matches(case_sensitive = False):
+    matchlist = open_matches()
+    elos = open_elos()
+    while True:
+        name1 = raw_input("enter first name: ")
+        name2 = raw_input("enter second name: ")
+        if name1 == 'q' and name2 == 'q':
+            break
+        faceoff = False
+        firstsecond = ([],[])
+        for match in matchlist:
+            #if both contestants have battled previously
+            if name1 in match and name2 in match:
+                faceoff = True
+                print match[2] + " won!!!!!!!!!!!!!!"
+                firstsecond[0].append(match)
+                firstsecond[1].append(match)
+            elif name1 in match:
+                firstsecond[0].append(match)
+            elif name2 in match:
+                firstsecond[1].append(match)
+        #if both contestants have not battled previously
+        if faceoff is False:
+            print "-------------"
+            print name1 + " has not fought " + name2 + "."
+        for i in 0,1:
+            print "------------"
+            wins = 0
+            contestant = (name1,name2)[i]
+            for match in firstsecond[i]:
+                try:
+                    if match[0] == match[2]:
+                        winner = 0
+                    else:
+                        winner = 1
+                    if match[winner] == contestant:
+                        wins+=1
+                    print match[2] + " beat " + match[1-winner]
+                except:
+                    print match
+            try:
+                winpercent = round(float(wins)/len(firstsecond[i]),1)
+                rndelo = round(get_elo(contestant,elos),2)
+            except:
+                print contestant + " is new!"
+                winpercent = -1
+                rndelo = 1600
+            print str(wins) + "/" + str(len(firstsecond[i])) + " : " + str(winpercent)
+            print contestant + "'s ELO RATING: " + str(rndelo)
+        print ""
+        
+def elo_matches():
+    """
+    The ELO System:
+
+    All new players start out with a base rating of 1600
+    WinProbability = 1/(10^(( Opponent’s Current Rating–Player's Current Rating)/400) + 1)
+    ScoringPt = 1 point if they win the match, 0 if they lose, and 0.5 for a draw.
+    Player's New Rating = Player's Old Rating + (K-Value * (ScoringPt–Player's Win Probability))
+    """
+    kval = 16
+    playerlist = {}
+    matchlist = open_matches()
+    
+    for match in matchlist:
+        for i in 0,1:
+            player = match[i]
+            if player not in playerlist:
+                playerlist[player] = 1600
+        for i in 0,1:
+            player = match[i]
+            winprob = 1/(pow(10,(playerlist[match[1-i]]-playerlist[player])/400) + 1)
+            try:
+                if player == match[2]:
+                    scoringpt = 1
+                else:
+                    scoringpt = 0
+            except:
+                scoringpt = .5
+            playerlist[player] = playerlist[player] + (kval * (scoringpt - winprob))
+            #print player + " new rating is " + str(playerlist[match[i]])
+    return playerlist
+
+def save_elos():
+    elos = elo_matches()
+    sorted_elos = OrderedDict(sorted(elos.items(),key = lambda x: x[1]))
+    with open("elos",'w') as p:
+        pickle.dump(sorted_elos,p)
+    print "elos saved!"
+
+def open_elos():
+    with open("elos",'r') as p:
+        elos = pickle.load(p)
+    return elos
+
+def print_elos(batches = 100):
+    try:
+        sorted_elos = open_elos()
+    except:
+        save_elos()
+        sorted_elos = open_elos()
+    j = 0
+    for i in reversed(sorted_elos):
+        j+=1
+        print i + ": " + str(round(sorted_elos[i],2))
+        if j%batches == 0:
+            if raw_input('go on?') == 'n':
+                break
+
+def get_elo(contestant,elos=None):
+    if elos is None:
+        elos = open_elos()
+    return elos[contestant]
+
+def find_elos():
+    elos = open_elos()
+    while True:
+        contestant = raw_input('Contestant name? ')
+        print contestant + ": " + str(round(elos[contestant],2))
+
+def save_all():
+    save_stats()
+    save_elos()
